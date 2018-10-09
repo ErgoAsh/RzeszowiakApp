7aa2a61f6fa4aee69f73027bb847e58c5aebf68d
LAPTOP-C4M3GNA0\Tomasz <tomkowa64@gmail.com>
2018-10-09 14:29:30 +0200

    Side Bard prototype - completed

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,17 @@ class App extends Component {
       <div className="sidenav">
        <h1>Kategoria: !KOMPONENT! </h1>
        <p>Cena</p>
+      od <input type="text" name="from" /><br />
+      do <input type="text" name="to" />
+      <p>Szukana fraza</p>
+      <input type="text" name="keywords" />
+      <p>Ogłoszenia z ostatnich</p>
+      <h3><input type="radio" name="date"/> 24 godzin</h3>
+      <h3><input type="radio" name="date"/> 3 Dni</h3>
+      <h3><input type="radio" name="date"/> 7 Dni</h3>
+      <h3><input type="radio" name="date"/> 14 Dni</h3>
+      <h3><input type="radio" name="date"/> 30 Dni</h3>
       </div>
-
     );
   }
 }
