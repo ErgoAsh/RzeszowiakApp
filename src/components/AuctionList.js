79457bfd6420cdea96d4df6351ea5ecf6770a835
LAPTOP-C4M3GNA0\Tomasz <tomkowa64@gmail.com>
2018-10-09 14:55:54 +0200

    Making components

---
+++ src/components/AuctionList.js
@@ -0,0 +1,13 @@
+import AuctionListItem from './AuctionListItem.js';
+import React, { Component } from 'react';
+
+class AuctionList extends Component {
+    render() {
+      return (
+        <ul>
+        </ul>
+      );
+    }
+  }
+  
+  export default AuctionList;
---
+++ src/components/AuctionListItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+class AuctionListItem extends Component {
+    render() {
+      return (
+        <div className="container">
+            <div className="ItemImage">
+
+
+            </div>
+            <div className="ItemInfo">
+
+                
+            </div>
+            <div className="ItemDescription">
+                <p>Lorem</p>
+                
+            </div>
+        </div>
+      );
+    }
+  }
+  
+  export default AuctionListItem;
