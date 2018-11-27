f385cae45994fcca2d883fb06c153f3e4dd3720b
Marek K <wiedzmin1371@gmail.com>
2018-11-27 10:32:09 +0100

    Bugfixing

--- webpack.config.js
+++ webpack.config.js
@@ -75,6 +75,12 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+              test: /\.(png|svg|jpg|gif)$/,
+              use: [
+                'file-loader'
+              ]
             }
         ]
     }
